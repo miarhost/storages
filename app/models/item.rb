2470329206a2ffb0977b0bc1c4ebb465d3f0c2ad
MariaTikhonova <ritz.rkraft@gmail.com>
2018-11-20 17:44:39 +0200

    some main views

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
  def item_params
   require_params = params.require(:item).permit(:attachment)
-  require_params[:attachment = item_params[:attachment].present? ? UploadIO.new(item_params[:attachment].tempfile, item_params[:attachment].content_type, iteml_params[:attachment].original_filename) : nil
+  require_params[:attachment] = item_params[:attachment].present? ? UploadIO.new(item_params[:attachment].tempfile, item_params[:attachment].content_type, iteml_params[:attachment].original_filename) : nil
   require_params
  end
 
