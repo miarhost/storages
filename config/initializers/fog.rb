afc4f2aa83c4581a1db7ccd3ad0531ef904279b9
MariaTikhonova <ritz.rkraft@gmail.com>
2018-12-25 12:52:40 +0200

    moving to aws s3

---
+++ config/initializers/fog.rb
@@ -0,0 +1,10 @@
+config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["ACCESS_KEY_ID"],                       # required
+    aws_secret_access_key: ENV["SECRET_ACCESS_KEY"],                        # required
+    region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
+    host:                  'https://s3.eu-west-2.amazonaws.com',             # optional, defaults to nil
+    endpoint:              'https://s3.eu-west-2.amazonaws.com' # optional, defaults to nil
+  }
+
+  
