bcf275107c59a4b75ff535b75036fafc24747068
MariaTikhonova <ritz.rkraft@gmail.com>
2018-12-29 17:42:06 +0200

    google oauth fixes in devise. some specs

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+      user = ::User.from_omniauth(oauth_response)
       if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
+        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider)
+        sign_in_and_redirect user, event: :authentication
       else
-        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) 
+        session['devise.google_data'] = oauth_response.except(:extra)
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
       end
     end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+   	subject { Item.new{attachment: "content.jpg", views: 0}}
+  end
+
+  describe "ActiveRecord relations" do
+  	 it "has to belong to folder" do
+  	 	 expect(subject).to belong_to(:folder)
+  	 end
+  	 it "has to have one attachment" do
+  	 	 expect(subject).to have_one(:attachment)
+  	 end
+  end
+  
 end
