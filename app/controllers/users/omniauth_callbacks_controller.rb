7b7b9c21a851c1850869cbd45aa2e941837cb4ab
MariaTikhonova <ritz.rkraft@gmail.com>
2018-12-30 22:19:55 +0200

    fix google oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,8 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
       user = ::User.from_omniauth(oauth_response)
       if @user.persisted?
-        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider)
-        sign_in_and_redirect user, event: :authentication
+        sign_in sign_in @user, :event => :authentication
+        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: "Google")
       else
         session['devise.google_data'] = oauth_response.except(:extra)
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
