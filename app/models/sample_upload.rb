d1843de8e35db9c36b14e10373d43e357b786228
MariaTikhonova <ritz.rkraft@gmail.com>
2019-01-21 15:57:51 +0200

    2 url showing helpers for s3 link in prod

--- app/models/sample_upload.rb
+++ app/models/sample_upload.rb
@@ -1,6 +1,14 @@
 class SampleUpload < ApplicationRecord
-	include Geturlable
+
+	  include Rails.application.routes.url_helpers
+
 	 belongs_to :sample_folder, optional: true
 	 has_one_attached :attachment
 	 scope :with_eager_loaded_attachment, -> { eager_load(attachment_attachment: :blob) }
+
+  def cover_url 
+    rails_blob_path(self.cover, disposition: "attachment", only_path: true)
+  end
+
+
 end
