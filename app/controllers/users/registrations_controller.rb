d0b9f34910642d37e8681b9e6155f2a1dbe9dad8
MariaTikhonova <ritz.rkraft@gmail.com>
2019-01-25 22:20:13 +0200

    showing user's files and folders at user's dashboard

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+   before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_account_update_params, only: [:update]
+
+   def edit
+    super
+    @user.folders = Folder.where(:user_id => @user.id)
+   end
+
+end
