9ce00169682d4e20cd648faf72ca3a9828b52002
MariaTikhonova <ritz.rkraft@gmail.com>
2019-03-15 13:04:40 +0200

    new relic & style changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-   before_action :configure_sign_up_params, only: [:create]
-   before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
-   def edit
+  def edit
     super
     @user.folders = Folder.where(:user_id => @user.id)
-   end
-
+  end
 end
