995c5ba09605cded60fa724afdc6dcee3ecd1dba
MariaTikhonova <miarhost@gmail.com>
2019-10-31 23:11:28 +0200

    mailer spec

--- spec/mailers/sample_upload_mailer_spec.rb
+++ spec/mailers/sample_upload_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe SampleUploadMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#get_link" do
+  	 it "delivers new sample upload link" do
+  	 	 sample_upload = build(:sample_upload)
+  	 	 email = double { Faker::Internet::email }
+  	 	 mail = SampleUploadMailer.get_link(email)
+      expect(mail).to deliver_to(email)
+      expect(mail).to deliver_from("rghostme@gmail.com")
+      expect(mail).to have_subject("Your upload link")
+      expect(email).to have_body_text("Hello, you uploaded a file")
+    end
+  end
 end
